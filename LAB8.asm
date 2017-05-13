6e04eb865ed99e780c5f8fd3096bd3b11e0758d5
Larry Liu <yunpengliu199703@gmail.com>
2017-05-12 20:36:22 -0700

    'init'

---
+++ LAB8.asm
@@ -0,0 +1,10 @@
+	.include "address_map_arm.s"
+	.text 
+	.global _start
+_start:
+		LDR R0, =SW_BASE
+		LDR r1, =LEDR_BASE
+L1:		LDR r2,[R0]  // load the switch value
+		MOV R3, R2, LSL #1 // 
+		STR R3,[r1]
+		B L1
